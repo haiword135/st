4a15ef93eb61db7f8dff606008052fa9e062b793
Young <Young@young.com>
2023-02-23 21:50:13 +0800

    patch

--- config.def.h
+++ config.def.h
@@ -118,48 +118,47 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.6;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	[0] = "#000000",
+	[1] = "#ff5555",
+	[2] = "#50fa7b",
+	[3] = "#f1fa8c",
+	[4] = "#bd93f9",
+	[5] = "#ff79c6",
+	[6] = "#8be9fd",
+	[7] = "#bbbbbb",
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	[8]  = "#44475a",
+	[9]  = "#ff5555",
+	[10] = "#50fa7b",
+	[11] = "#f1fa8c",
+	[12] = "#bd93f9",
+	[13] = "#ff79c6",
+	[14] = "#8be9fd",
+	[15] = "#ffffff",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	[256] = "#ebdbb2", // foreground
+	[257] = "#282828", // background
+	[258] = "#ffffff", // cursor
 };
 
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
 
 /*
  * Default shape of cursor
